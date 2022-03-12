47717316f4caecb2890631e5057cda4fce9b4521
19UV <the19UV@gmail.com>
2022-03-11 20:39:28 -0500

    Implemented poor man's normal lighting for chunk fragment shader.

--- shaders/chunk_material/fragment.glsl
+++ shaders/chunk_material/fragment.glsl
@@ -7,5 +7,13 @@ in vec3 v_normal;
 out vec4 frag_color;
 
 void main() {
-	frag_color = vec4(v_color, 1);
+	const vec3 light_pos = vec3(-35, 35, -35);
+
+	vec3 light_normal = normalize(light_pos - v_position);
+
+	float dif = clamp(dot(v_normal, light_normal), 0., 1.);
+
+	// vec3 diffusion_color = pow(vec3(dif), vec3(0.75)); // Gamma Correction
+
+	frag_color = vec4(v_color * vec3(dif), 1);
 }
